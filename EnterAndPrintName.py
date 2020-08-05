6eccdb8cdb1c76d398fc4f25c7b96c5483becd57
EmilyE1996 <69228088+EmilyE1996@users.noreply.github.com>
2020-08-04 22:44:26 -0400

    Add files via upload

---
+++ EnterAndPrintName.py
@@ -0,0 +1,9 @@
+## write a program that does the following in order:
+## asks the user to enter his/her date of birth.
+## asks the user to enter his/her last name
+## prints out the user's last name and date of birth, in that order.
+
+birthDate = input("What is your date of birth?")
+lastName = input("what is your last name?")
+
+print("Name: " + lastName + "\nBirth Date: " + birthDate)
---
+++ IDLE_Enter_Name_And_Print.py
@@ -0,0 +1,9 @@
+## write a program that does the following in order:
+## asks the user to enter his/her date of birth.
+## asks the user to enter his/her last name
+## prints out the user's last name and date of birth, in that order.
+
+birthDate = input("What is your date of birth?\n")
+lastName = input("What is your last name?\n")
+
+print("Name: " + lastName + "\nBirth Date: " + birthDate)
